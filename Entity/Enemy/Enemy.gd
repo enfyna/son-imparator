4718ffdd8a39e4e6de6b3dde826dde384edfc9c7
enfyna <legendraslan@gmail.com>
2024-04-27 13:29:10 +0300

    Add basic player and enemy func

--- Entity/Enemy/Enemy.gd
+++ Entity/Enemy/Enemy.gd
@@ -1,1 +1,18 @@
-extends CollisionShape2D
+class_name Enemy extends CharacterBody2D
+
+@export var target : Node2D
+@export var health : int
+@onready var health_bar : ProgressBar = $HealthBar
+
+func _ready() -> void:
+    health_bar.value = health
+
+func hit(damage:int):
+    health -= damage
+    if health <= 0:
+        die()
+    print(health)
+    health_bar.value = health
+
+func die():
+    queue_free()
--- Entity/Player/Player.gd
+++ Entity/Player/Player.gd
@@ -1,1 +1,34 @@
-extends CharacterBody2D
+class_name Player extends CharacterBody2D
+
+var limbs : Array[Limb] = []
+var direction : int = 0
+
+func _ready() -> void:
+    const limb_path = 'res://Entity/Item/Limbs/Player/'
+    const ability_path = 'res://Entity/Ability/Default'
+    var dir = DirAccess.open(limb_path)
+    for limb_file in dir.get_files():
+        var limb : Limb = load(limb_path + limb_file)
+        if limb.ability_name.length() > 0:
+            var ability_p = ability_path + "/" + limb.ability_name + ".tscn"
+            if FileAccess.file_exists(ability_p):
+                var ability : Ability = load(ability_p).instantiate()
+                ability.parent = self
+                ability.is_player = true
+                limb.ability = ability
+        limbs.append(limb)
+
+func _process(delta: float) -> void:
+    if Input.is_key_pressed(KEY_D):
+        direction = 0
+    elif Input.is_key_pressed(KEY_A):
+        direction = 1
+    if Input.is_key_pressed(KEY_S):
+        direction = 2
+    elif Input.is_key_pressed(KEY_W):
+        direction = 3
+
+    if Input.is_action_just_pressed('e'):
+        limbs[3].ability.activate(direction)
+    elif Input.is_action_just_pressed('q'):
+        limbs[3].ability.activate(direction)
