f3d0ec709f69fa32d444c7627ad75925c11f256e
enfyna <legendraslan@gmail.com>
2024-04-27 13:28:16 +0300

    Add Ability class

--- Entity/Ability/Ability.gd
+++ Entity/Ability/Ability.gd
@@ -7,6 +7,33 @@ enum TYPE{
     RANGED_MULTI,
 }
 
-var type : TYPE
-var damage : int = 10
-var aoe : DamageArea
+@export var type : TYPE
+@export var cooldown : float = 1.0
+@export var damage : int = 10
+@export var aoe : DamageArea
+var active_attack = false
+
+var is_player = false
+var parent : Node2D
+
+func activate(direction : int):
+    if active_attack:
+        return
+    var dmg : DamageArea = aoe.duplicate()
+    dmg.time = cooldown
+    dmg.is_player = is_player
+    dmg.damage = damage
+    dmg.connect('end', func(): active_attack = false)
+    if direction == 0:
+        dmg.position.x += 129
+    elif direction == 1:
+        dmg.position.x -= 129
+    elif direction == 2:
+        dmg.position.y += 129
+    elif direction == 3:
+        dmg.position.y -= 129
+    
+    active_attack = true
+    parent.add_child(dmg)
+    
+
